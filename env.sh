1425f5c04f8b408a6e06e0175367aa9ed08d0440
Florian Märkl <info@florianmaerkl.de>
2019-02-24 16:39:27 +0100

    Fix env.sh

--- env.sh
+++ env.sh
@@ -4,10 +4,10 @@ CUTTER_DEPS_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pw
 
 export CUTTER_DEPS_PYTHON_PREFIX="$CUTTER_DEPS_ROOT/python"
 
-export LD_LIBRARY_PATH="$CUTTER_DEPS_ROOT/qt/lib:`pwd`/python/lib:`pwd`/pyside/lib:$LD_LIBRARY_PATH"
-export PATH="$CUTTER_DEPS_ROOT/qt/bin:`pwd`/python/bin:`pwd`/pyside/bin:$PATH"
+export LD_LIBRARY_PATH="$CUTTER_DEPS_ROOT/qt/lib:$CUTTER_DEPS_ROOT/python/lib:$CUTTER_DEPS_ROOT/pyside/lib:$LD_LIBRARY_PATH"
+export PATH="$CUTTER_DEPS_ROOT/qt/bin:$CUTTER_DEPS_ROOT/python/bin:$CUTTER_DEPS_ROOT/pyside/bin:$PATH"
 
-export PKG_CONFIG_PATH="$CUTTER_DEPS_ROOT/qt/lib/pkgconfig:`pwd`/python/lib/pkgconfig:`pwd`/pyside/lib/pkgconfig"
+export PKG_CONFIG_PATH="$CUTTER_DEPS_ROOT/qt/lib/pkgconfig:$CUTTER_DEPS_ROOT/python/lib/pkgconfig:$CUTTER_DEPS_ROOT/pyside/lib/pkgconfig"
 
 # For QMake
 export QTDIR="$CUTTER_DEPS_ROOT/qt"
